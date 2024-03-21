c3152075e0952c9db5e29a4af2f44ec482469d26
ChenghengLi <80062217+ChenghengLi@users.noreply.github.com>
2024-03-21 08:49:50 -0700

    Update openai.js

--- src/apis/openai.js
+++ src/apis/openai.js
@@ -1,6 +1,6 @@
 import OpenAI from "openai";
 
-const API_KEY = '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';
+const API_KEY = 'RANDOM_API_KEY';
 
 const openai = new OpenAI({ apiKey: API_KEY , dangerouslyAllowBrowser: true });
 const actions = {
